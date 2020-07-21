701ae0da595fea37798afadf3c92226c8ca7334b
Stuart <contactstuartonline@gmail.com>
2020-07-21 15:58:21 +0100

    Remove backend functionality

--- src/App.js
+++ src/App.js
@@ -129,26 +129,12 @@ class App extends Component {
          <Particles className='particles'
           params={particlesOptions}
         />
-        {/* <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} /> */}
-        {/* { route === 'home' */}
-          ? <div>
-              <Logo />
-              {/* <Rank
-                name={this.state.user.name}
-                entries={this.state.user.entries}
-              /> */}
               <ImageLinkForm
                 onInputChange={this.onInputChange}
                 onButtonSubmit={this.onButtonSubmit}
               />
               <FaceRecognition box={box} imageUrl={imageUrl} />
             </div>
-          {/* : (
-             route === 'signin'
-            //  ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
-            //  : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
-            ) */}
-        {/* } */}
       </div>
     );
   }
